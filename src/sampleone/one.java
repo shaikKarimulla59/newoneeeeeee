9162bb107b5055c7bf3eb16d758755c7fcf14115
shaikKarimulla59 <karimulla1420626@gmail.com>
2024-07-10 17:17:11 +0530

    updated

---
+++ src/sampleone/one.java
@@ -0,0 +1,13 @@
+package sampleone;
+
+public class one {
+
+	public static void main(String[] args) {
+		System.out.println("one is printed");
+		System.out.println("one is printed");
+		System.out.println("one is printed");
+		System.out.println("one is printed");
+
+	}
+
+}
---
+++ src/sampleone/two.java
@@ -0,0 +1,13 @@
+package sampleone;
+
+public class two {
+
+	public static void main(String[] args) {
+		System.out.println("twooo is printed");
+		System.out.println("twooo is printed");
+		System.out.println("twooo is printed");
+		System.out.println("twooo is printed");
+
+	}
+
+}
